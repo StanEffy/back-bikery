21e3f243ab533378c631f7ee18314da6bfcce520
Stan Effy <holydonk@gmail.com>
2022-11-24 15:32:40 +0200

    rewrite errors ts

--- utils/appError.ts
+++ utils/appError.ts
@@ -1,9 +1,13 @@
 class AppError extends Error {
-  constructor(message, statusCode) {
+  isOperational: boolean;
+  status: string;
+  statusCode: string;
+
+  constructor(message: string, statusCode: string) {
     super(message);
 
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
