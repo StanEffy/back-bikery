a4d2b6336e8ab9da316d7362b6f83d956ce1225b
Stan Effy <holydonk@gmail.com>
2023-02-02 22:23:09 +0200

    add info to readme

--- controllers/stationController.ts
+++ controllers/stationController.ts
@@ -1,8 +1,7 @@
 import Station from '../models/stationModel';
-import {getAll, getOne, createOne, updateOne, deleteOne} from './handlerFactory';
+import {getAll, getOne, createOne, updateOne} from './handlerFactory';
 
 export const getAllStations = getAll(Station);
 export const getStation = getOne(Station);
 export const createStation = createOne(Station);
 export const updateStation = updateOne(Station);
-export const deleteStation = deleteOne(Station);
