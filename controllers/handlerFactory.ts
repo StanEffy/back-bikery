5cb3daeca8057371f00eba4214c7cd5bfaa4e2bf
Stan Effy <holydonk@gmail.com>
2023-02-03 12:28:42 +0200

    add types for handleFactory and change types in types file

--- controllers/handlerFactory.ts
+++ controllers/handlerFactory.ts
@@ -1,130 +1,134 @@
-// @ts-nocheck
-import catchAsync from '../utils/catchAsync';
-import AppError from '../utils/appError';
-import APIFeatures from '../utils/apiFeatures';
-import searchTopStations, {stationType} from "../utils/searchTopStations";
+import catchAsync from "../utils/catchAsync"
+import AppError from "../utils/appError"
+import APIFeatures from "../utils/apiFeatures"
+import {Model} from "mongoose"
+import {IStation, IStationStats, IStationToStation, ITrip} from "../models/types"
 
-export function deleteOne(Model) {
-    return catchAsync(async (req, res, next) => {
-        const doc = await Model.findByIdAndDelete(req.params.id);
+type ModelTripOrStation = Model<IStation> | Model<ITrip>
+type ModelAll = Model<IStation> | Model<ITrip> | Model<IStationToStation> | Model<IStationStats>
 
-        if (!doc) {
-            return next(new AppError('No document found with that ID', 404));
-        }
+export function deleteOne(Model: ModelTripOrStation) {
+	return catchAsync(async (req, res, next) => {
+		const doc = await Model.findByIdAndDelete(req.params.id)
 
-        res.status(204).json({
-            status: 'success',
-            data: null,
-        });
-    });
+		if (!doc) {
+			return next(new AppError("No document found with that ID", 404))
+		}
+
+		res.status(204).json({
+			status: "success",
+			data: null,
+		})
+	})
 }
 
 /**
  * @param {{ findById: (arg0: string) => any; }} Model
  * @param {any} popOptions
  */
-export function getOne(Model, popOptions = "") {
-    return catchAsync(async (req, res, next) => {
-        let reqId = req.params.id;
-        let station_id = req.params.station_id
+export function getOne(Model:ModelTripOrStation, popOptions = "") {
+	return catchAsync(async (req, res, next) => {
+		const reqId = req.params.id
+		const station_id = req.params.station_id
 
-        let query;
+		let query
 
-        if(reqId) query = Model.findById(reqId);
+		if(reqId) query = Model.findById(reqId)
 
-        if(station_id) {
-            console.log("We are inside")
-           const features = new APIFeatures(Model.find({station_id: station_id}), "")
-                .filter()
-                .sort()
-                .limitFields()
-                .paginate();
+		if(station_id) {
+			console.log("We are inside")
+			const features = new APIFeatures(Model.find({station_id: station_id}), "")
+				.filter()
+				.sort()
+				.limitFields()
+				.paginate()
 
-            const doc = await features.query;
+			const doc = await features.query
 
-            if (!doc) {
-                return next(new AppError('No document found with that ID', 404));
-            }
+			if (!doc) {
+				return next(new AppError("No document found with that ID", 404))
+			}
 
-            res.status(200).json({
-                status: 'success',
-                data: {
-                    data: doc,
-                },
-            });
-        }
+			res.status(200).json({
+				status: "success",
+				data: {
+					data: doc,
+				},
+			})
+		}
 
 
-        if (popOptions) query = query.populate(popOptions);
-        const doc = await query;
+		if (popOptions) query = query.populate(popOptions)
+		const doc = await query
 
-        if (!doc) {
-            return next(new AppError('No document found with that ID', 404));
-        }
+		if (!doc) {
+			return next(new AppError("No document found with that ID", 404))
+		}
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                data: doc,
-            },
-        });
-    });
+		res.status(200).json({
+			status: "success",
+			data: {
+				data: doc,
+			},
+		})
+	})
 }
 
-export function getAll(Model) {
-    return catchAsync(async (req, res, next) => {
-        let filter = {};
-        if (req.params.stationId) filter = { $or: [{departure_station_id: req.params.stationId}, { return_station_id: req.params.stationId}]};
+export function getAll(Model: ModelAll) {
+	return catchAsync(async (req, res, _next) => {
+		const filter = {}
 
-        const features = new APIFeatures(Model.find(filter), req.query)
-            .filter()
-            .sort()
-            .limitFields()
-            .paginate();
-        // const doc = await features.query.explain();
-        const doc = await features.query;
+		if (req.params.stationId) filter = { $or: [{departure_station_id: req.params.stationId}, { return_station_id: req.params.stationId}]}
 
-        res.status(200).json({
-            status: 'success',
-            results: doc.length,
-            data: {
-                data: doc,
-                // stats: statsObject
-            },
-        });
-    });
+		const features = new APIFeatures(Model.find(filter), req.query)
+			.filter()
+			.sort()
+			.limitFields()
+			.paginate()
+		// const doc = await features.query.explain();
+		const doc = await features.query
+
+		res.status(200).json({
+			status: "success",
+			results: doc.length,
+			data: {
+				data: doc,
+				// stats: statsObject
+			},
+		})
+	})
 }
 
-export function updateOne(Model) {
-    return catchAsync(async (req, res, next) => {
-        const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
+export function updateOne(Model: ModelAll) {
+	return catchAsync(async (req, res, next) => {
+		const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		})
 
-        if (!doc) {
-            return next(new AppError('No document found with that ID', 404));
-        }
+		if (!doc) {
+			return next(new AppError("No document found with that ID", 404))
+		}
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                data: doc,
-            },
-        });
-    });
+		res.status(200).json({
+			status: "success",
+			data: {
+				data: doc,
+			},
+		})
+	})
 }
 
-export function createOne(Model) {
-    return catchAsync(async (req, res, next) => {
+export function createOne(Model: ModelAll) {
+	return catchAsync(async (req, res, _next) => {
 
-        const doc = await Model.create(req.body);
+		const doc = await Model.create(req.body)
 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                data: doc,
-            },
-        });
-    });
+		res.status(201).json({
+			status: "success",
+			data: {
+				data: doc,
+			},
+		})
+	})
 }
--- utils/searchTopStations.ts
+++ utils/searchTopStations.ts
@@ -1,24 +1,24 @@
-//@ts-nocheck
 export enum stationType {
     "departure",
     "return"
 }
-const searchTopStations = (arrayOfTrips, mainStationType: stationType) => {
-    const res = {}
-    arrayOfTrips.forEach((trip) => {
-        switch (mainStationType) {
-            case stationType.departure:
-            {
-               return res[trip.return_station_name] ? res[trip.return_station_name]++ : res[trip.return_station_name] = 1
-            }
-            case stationType.return:
-            {
-                return res[trip.departure_station_name] ? res[trip.departure_station_name]++ : res[trip.departure_station_name] = 1
-            }
-        }
-    })
 
-    return res
+const searchTopStations = (arrayOfTrips, mainStationType: stationType) => {
+	const res = {}
+	arrayOfTrips.forEach((trip) => {
+		switch (mainStationType) {
+		case stationType.departure:
+		{
+			return res[trip.return_station_name] ? res[trip.return_station_name]++ : res[trip.return_station_name] = 1
+		}
+		case stationType.return:
+		{
+			return res[trip.departure_station_name] ? res[trip.departure_station_name]++ : res[trip.departure_station_name] = 1
+		}
+		}
+	})
+
+	return res
 }
 
-export default searchTopStations;
+export default searchTopStations
