12726b4560c2fb010c3c731cb9129ed760b72d21
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2023-02-05 13:42:35 +0200

    Delete build directory

--- build/utils/appError.d.ts
+++
@@ -1,7 +0,0 @@
-declare class AppError extends Error {
-    isOperational: boolean;
-    status: string;
-    statusCode: number;
-    constructor(message: string, statusCode: number);
-}
-export default AppError;
--- build/utils/catchAsync.d.ts
+++
@@ -1,3 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-declare const _default: (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => (req: Request, res: Response, next: NextFunction) => void;
-export default _default;
